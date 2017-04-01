921b66c61e6c59cae45f2d845e7c0d86116294b2
Vatan <vatanaksoytezer@gmail.com>
2017-04-01 15:12:46 +0300

    Add writing function

--- Raspberry/serial_communication.py
+++ Raspberry/serial_communication.py
@@ -5,58 +5,74 @@ import time
 from glob import glob
 import serial
 
-ports = glob('/dev/ttyUSB*')
+
+ports = glob('/dev/ttyACM*')
 serials = []
 utilities = {}
-utility_names = {"SC": "Sensor Controller", "BM": "Battery Management",
-                 "RA": "Robotic Arm", "MC": "Motor Controller",
-                 "LR": "LORA"}
+utility_names = {"S": "Sensor Controller", "B": "Battery Management",
+                 "R": "Robotic Arm", "M": "Motor Controller",
+                 "L": "LORA"}
 
 
 def read_data(utility_name):
     utility = utilities[utility_name]
-
     if utility.inWaiting:
         utility.flush()
         data_array = utility.readline().split("\n")
         utility_data = str(data_array[0])
 
-        return utility_data
+        print(utility_name + " says:" + utility_data)
+
+def write_data(utility_name):
+	utility = utilities[utility_name]
+	utility.write("B150000E")
+
 
 for port in ports:
     try:
-        serials.append(serial.Serial(port, 9600))
+        serials.append(serial.Serial(port, 115200))
         print(port)
     except (OSError, serial.SerialException):
         pass
 
+
 serial_count = 0
 num_serials = len(serials)
+
 
 print('Sleeping for 1 second for initializing')
 time.sleep(1)
 
+
 while serial_count != num_serials:
     serial_data = serials[serial_count].readline().split("\n")
     utility_name = str(serial_data[0])
+    serials[serial_count].write('BiE')
 
     if utility_name in utility_names:
         print(utility_name + " Identified")
         print("Connected to " + utility_names[utility_name] + ", initializing")
 
         utility = serials[serial_count]
-        utility.write('i')
         time.sleep(.5)
 
         utility.flushInput()
         utility.flushOutput()
         utilities[utility_name] = utility
 
-        serial_count += 1
+        serial_count +=1
+
 
 while True:
     for utility_name in utility_names:
         try:
-            print(read_data(utility_name))
+            read_data(utility_name)
         except Exception:
             pass
+
+    for utility_name in utility_names:
+        try:
+            write_data(utility_name)
+        except Exception:
+            pass
+
