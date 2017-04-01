c3618650ab9553bd347458ddb074ff81bb1e04cf
alperkesen <kesen.alper@gmail.com>
2017-04-01 19:29:51 +0300

    fix broken pipe error

--- Raspberry/tcp_server.py
+++ Raspberry/tcp_server.py
@@ -5,17 +5,15 @@ import socket
 import time
 import atexit
 
+
+@atexit.register
 def exit_handler():
-   clientsocket.close()
-   print("Server closing")
-atexit.register(exit_handler)
-
-# create a socket object
-serversocket = socket.socket(
-                socket.AF_INET, socket.SOCK_STREAM)
+    clientsocket.close()
+    print("Server closing")
 
 
-# serversocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
 # get local machine name
 host = ''
 
@@ -24,16 +22,20 @@ port = 1234
 # bind to the port
 serversocket.bind((host, port))
 
-# queue up to 5 requests
-serversocket.listen(5)
 
-# establish a connection
-# This should be in while if more clients are available
-clientsocket,addr = serversocket.accept()
-print("Got a connection from %s" % str(addr))
 while True:
-    sensor_msg = "12,12,12,12,12,12" + "\r"
-    clientsocket.send(sensor_msg.encode('ascii'))
-    data = clientsocket.recv(1024)
-    print(data)
-    time.sleep(.5)
+    try:
+        serversocket.listen(5)
+        clientsocket, addr = serversocket.accept()
+        print("Got a connection from %s" % str(addr))
+        while True:
+            try:
+                sensor_msg = "12,12,12,12,12,12" + "\n"
+                clientsocket.send(sensor_msg.encode('ascii'))
+                data = clientsocket.recv(1024)
+                print(data)
+                time.sleep(.5)
+            except Exception:
+                break
+    except Exception:
+        pass
