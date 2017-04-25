a9fdfdc227d955240391dc03cefbd1ee2758a9c6
alperkesen <kesen.alper@gmail.com>
2017-04-25 20:38:39 +0300

    add logging to client file

--- Raspberry/client.py
+++ Raspberry/client.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import asyncore
+import logging
 import socket
 import time
 from settings import *
@@ -11,30 +12,44 @@ class Client(asyncore.dispatcher):
     def __init__(self, host, port):
         asyncore.dispatcher.__init__(self)
         self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
+        logging.info('Create client')
         self.connect((host, port))
         self.msg = "Hello, I'm Client"
+        self.is_writable = True
 
     def handle_connect(self):
+        logging.info('Client is connected')
         print "connected"
 
     def handle_close(self):
+        logging.info('Client is disconnected')
         print "closed"
         self.close()
 
     def handle_read(self):
-        print(self.recv(1024))
+        self.data = self.recv(1024)
+        print(self.data[:-1])
+        logging.info('Read: "%s"', self.data[:-1])
 
     def handle_error(self):
-        print "Server is closed..."
+        print("Server is closed...")
+        logging.error('Server is closed!')
 
     def writable(self):
-        return True
+        self.is_writable = not self.is_writable
+        return self.is_writable
 
     def handle_write(self):
         sent = self.send(self.msg)
-        time.sleep(1)
+        logging.info('Send a message: "%s"', self.msg)
 
 
 if __name__ == "__main__":
+    logging.basicConfig(filename='logs/client.log',
+                        filemode='a',
+                        format='%(asctime)s %(message)s',
+                        level=logging.INFO)
+    logging.info('Start')
     client = Client(HOST, PORT)
-    asyncore.loop()
+    asyncore.loop(timeout=1)
+    logging.info('Finish')
